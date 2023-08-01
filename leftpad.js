681c39e455598f72321c006c1d407f0210398a0c
Joao Marcos <46683846+siriusblck3@users.noreply.github.com>
2023-08-01 11:14:51 +0200

    console.log?

--- leftpad.js
+++ leftpad.js
@@ -37,7 +37,7 @@ const leftpad = (str, length, paddingChar = " ") => {
       paddingChar += paddingChar;
     } else break;
   }
-  return paddingChar + str;
+  console.log(paddingChar + str;)
 }
 
 export { leftpad };
