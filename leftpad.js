5090f207a9f57df1c7a7322c5b56807be19d07a3
Joao Marcos <46683846+siriusblck3@users.noreply.github.com>
2023-08-01 11:03:36 +0200

    Update leftpad.js

--- leftpad.js
+++ leftpad.js
@@ -1,49 +1,43 @@
 // leftpad.js
 var cache = [
-  '',
-  ' ',
-  '  ',
-  '   ',
-  '    ',
-  '     ',
-  '      ',
-  '       ',
-  '        ',
-  '         '
+  "",
+  " ",
+  "  ",
+  "   ",
+  "    ",
+  "     ",
+  "      ",
+  "       ",
+  "        ",
+  "         ",
 ];
 
-function leftpad(str, length, paddingChar = ' ') {
-
+function leftpad(str, length, paddingChar = " ") {
   length = length - str.length;
-
-  if (length <= 0 ) {
+  if (length <= 0) {
     return str;
   }
-
-  if(!paddingChar && paddingChar !== 0) {
-    paddingChar = ' ';
+  if (!paddingChar && paddingChar !== 0) {
+    paddingChar = " ";
   }
+  paddingChar = paddingChar + "";
 
-  paddingChar = paddingChar + '';
-
-  if(paddingChar === ' ' && length < 10) {
+  if (paddingChar === " " && length < 10) {
     return cache[length] + str;
   }
 
-  var pad = '';
-
-  while(true){
-    if(length & 1){
+  var pad = "";
+  while (true) {
+    if (length & 1) {
       pad += paddingChar;
     }
     length >>= 1;
-    
-    if(length){
-      paddingChar += paddingChar;
-    }
-  }
 
-  return pad + str;
+    if (length) {
+      paddingChar += paddingChar;
+    } else break;
+  }
+  return paddingChar + str;
 }
 
-export {leftpad};
+export { leftpad };
